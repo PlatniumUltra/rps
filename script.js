d6c2dd75b1b924d7248d9d000af75be70db462b2
Nadir <nadirmsharif@gmail.com>
2022-03-16 16:45:56 -0400

    ui

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
-
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
+    const result = document.querySelector('#result');
+    const score = document.querySelector('#score');
+
+    score.textContent = score.textContent || 0
 
     if (playerSelection == "rock" && computerSelection == "paper") {
-        return "You Lose! Paper beats Rock";
+        result.textContent  = "You Lose! Paper beats Rock";
+        score.textContent = parseInt(score.textContent) - 1;
     } else if (playerSelection == "paper" && computerSelection == "scissor") {
-        return "You Lose! scissor beats paper";
+        result.textContent  = "You Lose! scissor beats paper";
+        score.textContent = parseInt(score.textContent) - 1;
     } else if (playerSelection == "scissor" && computerSelection == "rock") {
-        return "You Lose! rock beats scissor";
+        result.textContent  = "You Lose! rock beats scissor";
+        score.textContent = parseInt(score.textContent) - 1;
     } else if (playerSelection == "rock" && computerSelection == "scissor") {
-        return "You Win! Rock beats scissor";
+        result.textContent  = "You Win! Rock beats scissor";
+        score.textContent = parseInt(score.textContent) + 1;
     } else if (playerSelection == "scissor" && computerSelection == "paper") {
-        return "You Win! scissor beats paper"
+        result.textContent  = "You Win! scissor beats paper";
+        score.textContent = parseInt(score.textContent) + 1;
     } else if (playerSelection == "paper" && computerSelection == "rock") {
-        return "You Win! paper beats rock";
+        result.textContent  = "You Win! paper beats rock";
+        score.textContent = parseInt(score.textContent) + 1;
     } else {
-        return "Tie";
+        result.textContent  = "Tie";
     }
 
+    if(parseInt(score.textContent) == 5){
+        alert("You win")
+    }
+    if(parseInt(score.textContent) == -5){
+        alert("You lose")
+    }
 }
 
 function computerPlay() {
@@ -30,11 +45,19 @@ function computerPlay() {
     } else {
         return "scissor";
     }
-
-
 }
 
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
-console.log(computerSelection);
+// const playerSelection = "rock";
+// const computerSelection = computerPlay();
+// console.log(playRound(playerSelection, computerSelection));
+// console.log(computerSelection);
+
+
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll('button');
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+    // and for each one we add a 'click' listener
+    button.addEventListener('click',() => playRound(button.innerHTML,  computerPlay() ));
+});
